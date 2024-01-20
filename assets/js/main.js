415e8495143176247af008c89c6fb007c288a19c
ishubtripathi <ishubtripathi@gmail.com>
2024-01-20 23:09:36 +0530

    New Features

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,3 +33,5 @@ function scrollHeader(){
     if(this.scrollY >= 80) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
+
+// login logic

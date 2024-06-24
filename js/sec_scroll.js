0a66a5813ff50680ccd33c3d96905a95e21cb523
ishubtripathi <ishubtripathi@gmail.com>
2024-06-24 18:04:52 +0530

    new files

--- js/sec_scroll.js
+++ js/sec_scroll.js
@@ -1,22 +1,22 @@
-const container1 = document.querySelector(".container1");
-const sections = gsap.utils.toArray(".container1 section");
+const container = document.querySelector(".container");
+const sections = gsap.utils.toArray(".container section");
 const texts = gsap.utils.toArray(".anim");
 const mask = document.querySelector(".mask");
 
 let scrollTween = gsap.to(sections, {
-  xPercent: -100 * (sections.length - 1),
+  xPercent: - 100 * (sections.length - 1),
   ease: "none",
   scrollTrigger: {
-    trigger: ".container1",
+    trigger: ".container",
     pin: true,
     scrub: 1,
     end: "+=3000",
-    //snap: 1 / (sections.length - 1),
-    markers: true,
+    // snap: 1 / (sections.length - 1),
+    // markers: true,
   }
 });
 
-console.log(1 / (sections.length - 1))
+console.log(0.5 / (sections.length - 1))
 
 //Progress bar animation
 
@@ -25,7 +25,10 @@ gsap.to(mask, {
   scrollTrigger: {
     trigger: ".wrapper",
     start: "top left",
-    scrub: 1
+    stagger: 1,
+    scrub: 1,
+    // markers: true,
+    end: "+=3500",
   }
 });
 
@@ -36,19 +39,18 @@ sections.forEach((section) => {
   
   // bump out if there's no items to animate
   if(text.length === 0)  return 
-  
   // do a little stagger
   gsap.from(text, {
     y: -130,
     opacity: 0,
     duration: 2,
-    ease: "elastic",
+    ease: "none",
     stagger: 0.1,
     scrollTrigger: {
       trigger: section,
-      container1Animation: scrollTween,
+      // containerAnimation: scrollTween,
       start: "left center",
-      markers: true
+      // markers: true
     }
   });
 });
